062421e6ec0208df67cbe5853fcc9a07743faab4
taraswww777 <taraswww777@mail.ru>
2023-10-24 10:15:37 +0300

    antd Col Row

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import {Col, Divider, Row} from 'antd';
 import {Counter} from '../components/Counter';
 import {Pokemon} from '../components/Pokemon.tsx';
 import {Posts} from '../components/Posts.tsx';
@@ -6,16 +7,23 @@ import {AppLoader} from './AppLoader.tsx';
 export function App() {
   return (
     <AppLoader>
-      <h1>redux-toolkit</h1>
-      <div>
-        <Counter />
-      </div>
-      <div>
-        <Pokemon />
-      </div>
-      <div>
-        <Posts />
-      </div>
+      <Row gutter={16}>
+        <Col span={24}>
+          <h1>redux-toolkit</h1>
+        </Col>
+        <Col span={24}>
+          <Divider orientation="left">Counter</Divider>
+          <Counter />
+        </Col>
+        <Col span={24}>
+          <Divider orientation="left">Pokemon</Divider>
+          <Pokemon />
+        </Col>
+        <Col span={24}>
+          <Divider orientation="left">Posts</Divider>
+          <Posts />
+        </Col>
+      </Row>
     </AppLoader>
   )
 }

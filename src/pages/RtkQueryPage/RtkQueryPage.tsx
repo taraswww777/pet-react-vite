a370d222ce1dc137f646cf02a8c8afa2c145417e
taraswww777 <taraswww777@mail.ru>
2023-11-10 17:47:18 +0300

    PokemonListWithDetailSubPage

--- src/pages/RtkQueryPage/RtkQueryPage.tsx
+++ src/pages/RtkQueryPage/RtkQueryPage.tsx
@@ -2,26 +2,35 @@ import {Col, Divider, Row} from 'antd';
 import {Counter} from './components/Counter.tsx';
 import {Pokemon} from './components/Pokemon.tsx';
 import {Posts} from './components/Posts.tsx';
+import {PokemonListWithDetailSubPage} from './subPages/PokemonListWithDetailSubPage';
 
 const RtkQueryPage = () => {
   return (
     <Row gutter={16}>
-
       <Col span={24}>
         <h1>redux-toolkit</h1>
       </Col>
+      {false && (
+        <>
+          <Col span={24}>
+            <Divider orientation="left">Counter</Divider>
+            <Counter />
+          </Col>
+          <Col span={24}>
+            <Divider orientation="left">Pokemon</Divider>
+            <Pokemon />
+          </Col>
+          <Col span={24}>
+            <Divider orientation="left">Posts</Divider>
+            <Posts />
+          </Col>
+        </>
+      )}
 
       <Col span={24}>
-        <Divider orientation="left">Counter</Divider>
-        <Counter />
-      </Col>
-      <Col span={24}>
-        <Divider orientation="left">Pokemon</Divider>
-        <Pokemon />
-      </Col>
-      <Col span={24}>
-        <Divider orientation="left">Posts</Divider>
-        <Posts />
+        <Divider orientation="left">PokemonListWithDetailSubPage</Divider>
+
+        <PokemonListWithDetailSubPage />
       </Col>
     </Row>
   )
---
+++ src/pages/RtkQueryPage/subPages/PokemonListWithDetailSubPage.tsx
@@ -0,0 +1,3 @@
+export const PokemonListWithDetailSubPage = () => (
+  <p>werw</p>
+)
